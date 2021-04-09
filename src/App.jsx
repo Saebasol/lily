d1d5e7db51309e9ea17eb4f533c3eaa0cb238728
Ryu JuHeon <saidbysolo@gmail.com>
2021-04-09 22:51:15 +0900

    refactor(App): resolve all conversation

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,43 @@
-import React, { useState, useEffect } from 'react'
+import * as React from 'react'
 import JSZip from 'jszip'
 import saveAs from 'file-saver'
 import api from './api'
 
 
 function App() {
+  const { useState, useEffect } = React
 
   const [index, setIndex] = useState(0)
   const downloadIndex = window.location.pathname.replace("/", "")
 
   async function mainFunc() {
-    const zip = new JSZip()
-    const imageFolder = zip.folder(downloadIndex)
+    if (window.confirm("언어의 한계로 많은 이미지는 다운로드 할수 없을수도 있습니다. 시도하시겠습니까?")) {
+      const zip = new JSZip()
+      const imageFolder = zip.folder(downloadIndex)
 
-    const imageInfoResponse = await fetch(api + `api/hitomi/images/${downloadIndex}`)
-    if (imageInfoResponse.status == 404) {
-      alert("찾을수 없습니다.")
-      return
+      const imageInfoResponse = await fetch(api + `api/hitomi/images/${downloadIndex}`)
+      if (imageInfoResponse.status == 404) {
+        alert("찾을수 없습니다.")
+        return
+      }
+      const imagesInfo = await imageInfoResponse.json()
+
+      let count = 0
+
+      const downloadImage = imagesInfo.images.map(async (imageInfo, index) => {
+        const image = await fetch(imageInfo.url)
+        const imgBlob = await image.blob()
+
+        imageFolder.file(imageInfo.filename, imgBlob)
+        setIndex(count += 1)
+      })
+
+      await Promise.all(downloadImage).then(() =>
+        zip.generateAsync({ type: 'blob' })
+          .then(content => {
+            saveAs.saveAs(content, `${downloadIndex}.zip`)
+          }))
     }
-    const imagesInfo = await imageInfoResponse.json()
-
-    let count = 0
-
-    const downloadImage = imagesInfo.images.map(async (imageInfo, index) => {
-      const image = await fetch(imageInfo.url)
-      const imgBlob = await image.blob()
-
-      imageFolder.file(imageInfo.filename, imgBlob)
-      setIndex(count += 1)
-    })
-
-    await Promise.all(downloadImage).then(() =>
-      zip.generateAsync({ type: 'blob' })
-        .then(content => {
-          saveAs.saveAs(content, `${downloadIndex}.zip`)
-        }))
   }
 
   useEffect(() => {
